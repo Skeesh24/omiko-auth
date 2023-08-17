20da98ccdc39dde14cbe0a052ad6d4e5dd2cfeda
skeesh24 <d.pacooloff@gmail.com>
2023-08-17 21:05:17 +1000

    validation models added

---
+++ app/classes/validation.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from typing import Any
+from pydantic import BaseModel
+
+
+class TokenData(BaseModel):
+    email: str
+    created_at: datetime
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class FilterModel(BaseModel):
+    field_path: str
+    op_string: str
+    value: Any
+
+    def d(self) -> dict:
+        return self.model_dump(exclude_defaults=True)
+    
+    @staticmethod
+    def fast(path: str, value):
+        return FilterModel(field_path=path, op_string="==", value=value)
+    
+
+    
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserResponse(BaseModel):
+    id: str
+    email: str
+    password: str
