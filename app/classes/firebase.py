a1937ee17a4c5697ca7925c8736e05e5d0bfb48a
skeesh24 <d.pacooloff@gmail.com>
2023-08-18 21:49:17 +1000

    singlle quote cert text

--- app/classes/firebase.py
+++ app/classes/firebase.py
@@ -4,11 +4,7 @@ from os import environ, remove
 from fireo import db
 
 
-cert = """
-{
-    "type": "service_account", "project_id": "%s", "private_key_id": "%s", "private_key": "%s", "client_email": "%s", "client_id": "%s", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://oauth2.googleapis.com/token", "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", "client_x509_cert_url": "%s", "universe_domain": "googleapis.com"
-}
-""" % (
+cert = '{ "type": "service_account", "project_id": "%s", "private_key_id": "%s", "private_key": "%s", "client_email": "%s", "client_id": "%s", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://oauth2.googleapis.com/token", "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs", "client_x509_cert_url": "%s", "universe_domain": "googleapis.com"}' % (
     environ.get("AUTH_PROJECT_ID"),
     environ.get("AUTH_PRIVATE_KEY_ID"),
     environ.get("AUTH_PRIVATE_KEY"),
