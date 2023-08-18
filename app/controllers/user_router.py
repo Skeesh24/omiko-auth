1ee8bee1ae56429f2480242609f358e1fec4acee
skeesh24 <d.pacooloff@gmail.com>
2023-08-18 18:05:36 +1000

    usage of the userresponse model

--- app/controllers/user_router.py
+++ app/controllers/user_router.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Depends
 from fastapi import status
 
 from app.classes.oauth2 import get_current_user, get_hashed
-from app.classes.validation import UserCreate, UserBase
+from app.classes.validation import UserCreate, UserBase, UserResponse
 from app.classes.dependencies import get_users
 from app.classes.repository import UserFirebase
 from app.classes.validation import FilterModel
@@ -12,13 +12,13 @@ from app.classes.validation import FilterModel
 user_router = APIRouter(prefix="/user", tags=["user"])
 
 
-@user_router.get("")
+@user_router.get("", response_model=Union[List[UserResponse], UserResponse])
 async def get_all_users(limit: int = 5, offset: int = 0, db: UserFirebase = Depends(get_users)):
     users = db.get(limit=limit, offset=offset)
     return users
 
 
-@user_router.get("/{email:str}")
+@user_router.get("/{email:str}", response_model=UserResponse)
 async def get_user_by_email(email: str, db: UserFirebase = Depends(get_users)):
     user = db.get(limit=1, where=FilterModel.fast("email", email))
     return user
