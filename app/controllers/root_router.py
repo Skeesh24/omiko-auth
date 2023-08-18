0208a206fb01a0290661cd0ec30d8e9fca781da2
skeesh24 <d.pacooloff@gmail.com>
2023-08-18 22:28:49 +1000

    cors policy in test regime

--- app/controllers/root_router.py
+++ app/controllers/root_router.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.controllers.user_router import user_router
 from app.controllers.auth_router import auth_router
 
@@ -7,3 +9,10 @@ app = FastAPI()
 app.include_router(user_router)
 app.include_router(auth_router)
 
+app.add_middleware(
+        CORSMiddleware, 
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_headers=["*"],
+        allow_methods=["*"]
+    )
