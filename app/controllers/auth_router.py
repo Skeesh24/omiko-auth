bfa976389dd568d3191b691ee3654b4c7a2eedd2
skeesh24 <d.pacooloff@gmail.com>
2023-08-21 19:46:49 +1000

    usage of token response

--- app/controllers/auth_router.py
+++ app/controllers/auth_router.py
@@ -5,14 +5,14 @@ from fastapi.security import OAuth2PasswordRequestForm
 from app.classes.repository import UserFirebase
 from app.classes.dependencies import get_users
 from app.classes.oauth2 import create_access_token, verify
-from app.classes.validation import Token, FilterModel
+from app.classes.validation import TokenResponse, Token, FilterModel
 
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
-@auth_router.post("/login", status_code=status.HTTP_201_CREATED)
+@auth_router.post("/login", status_code=status.HTTP_201_CREATED, response_model=TokenResponse)
 async def login(credentials: OAuth2PasswordRequestForm = Depends(), db: UserFirebase = Depends(get_users)):
     user = db.get(limit=1, where=FilterModel.fast("email", credentials.username))
 
@@ -21,4 +21,7 @@ async def login(credentials: OAuth2PasswordRequestForm = Depends(), db: UserFire
 
     access_token = create_access_token({"email": user["email"]})
 
-    return Token(access_token=access_token, token_type="Bearer")
+    return TokenResponse(
+            token=Token(access_token=access_token, token_type="Bearer"),
+            user=user
+        )
