fddfeb2793924a6bb0439f6e142e90a12d56903b
skeesh24 <d.pacooloff@gmail.com>
2023-08-22 16:51:38 +1000

    dependencies refctoring

--- app/classes/dependencies.py
+++ app/classes/dependencies.py
@@ -1,5 +1,21 @@
-from app.classes.repository import UserFirebase
+from fastapi import Depends
+from fastapi_jwt_auth import AuthJWT
+
+from ..database.firebase.repository import UserFirebase
+from .validation import FilterModel, UserResponse
 
 
-async def get_users():
+def get_users():
     return UserFirebase()
+
+
+def get_current_user(
+    authorization: AuthJWT = Depends(), db: UserFirebase = Depends(get_users)
+) -> UserResponse:
+    authorization.jwt_required()
+
+    username = authorization.get_jwt_subject()
+
+    user = db.get(limit=1, where=FilterModel.fast("username", username))
+
+    return UserResponse(**user)
