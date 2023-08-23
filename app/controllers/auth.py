254f249e4638428409a078c8a42b04e37b51e5bb
skeesh24 <d.pacooloff@gmail.com>
2023-08-23 22:52:19 +1000

    logout login and caching using the cache service

--- app/controllers/auth.py
+++ app/controllers/auth.py
@@ -1,10 +1,11 @@
 from datetime import timedelta
-from fastapi import APIRouter, Depends, Form, HTTPException, Response, status
-from fastapi.responses import JSONResponse
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from fastapi_another_jwt_auth import AuthJWT
+
+from ..classes.interfaces import ICacheService
 from ..classes.crypto import verify
-from ..classes.dependencies import get_users
+from ..classes.dependencies import get_caching_service, get_users
 
 from ..database.firebase.repository import UserFirebase
 
@@ -18,7 +19,9 @@ from ..classes.validation import (
 from ..configuration import Settings
 
 
-auth_router = APIRouter(prefix="/auth", tags=["auth"])
+auth_router = APIRouter(
+    prefix="/auth", tags=["auth"], dependencies=[Depends(get_caching_service)]
+)
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/token")
@@ -29,9 +32,12 @@ async def login(
     credentials: OAuth2PasswordRequestForm = Depends(),
     Authorize: AuthJWT = Depends(),
     db: UserFirebase = Depends(get_users),
+    cache: ICacheService = Depends(get_caching_service),
 ):
     # username checks here
-    user = db.get(limit=1, where=FilterModel.fast("username", credentials.username))
+    user, success = cache.elem_and_status(credentials.username)
+    if not success:
+        user = db.get(limit=1, where=FilterModel.fast("username", credentials.username))
 
     # pass checks here
     if not verify(credentials.password, user["password"]):
@@ -39,12 +45,17 @@ async def login(
 
     access_token = Authorize.create_access_token(
         subject=credentials.username,
-        expires_time=timedelta(minutes=Settings().authjwt_access_token_expires)
+        expires_time=timedelta(minutes=Settings().authjwt_access_token_expires),
     )
     refresh_token = Authorize.create_refresh_token(
         subject=credentials.username,
-        expires_time=timedelta(days=Settings().authjwt_refresh_token_expires)
+        expires_time=timedelta(days=Settings().authjwt_refresh_token_expires),
     )
+
+    # caching is here
+    if not success:
+        cache.set(credentials.username + "_profile", user)
+    cache.set(credentials.username + "_token", refresh_token)
 
     return TokenResponse(
         tokens=TokenType(
@@ -57,15 +68,38 @@ async def login(
 
 
 @auth_router.post("/logout")
-async def logout(Auhthorize: AuthJWT = Depends()):
+async def logout(
+    Auhthorize: AuthJWT = Depends(), cache: ICacheService = Depends(get_caching_service)
+):
+    Auhthorize.jwt_refresh_token_required()
+
     # delete token from the database
-    pass
+    username = Auhthorize.get_jwt_subject()
+    if not cache.remove(username + "_token"):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="error deleting token"
+        )
 
 
 @auth_router.post("/refresh", response_model=AccessToken)
-async def refresh(Auhthorize: AuthJWT = Depends()):
+async def refresh(
+    Auhthorize: AuthJWT = Depends(), cache: ICacheService = Depends(get_caching_service)
+):
     Auhthorize.jwt_refresh_token_required()
-    # compare with the token in the database
 
-    access_token = Auhthorize.create_access_token(Auhthorize.get_jwt_subject())
+    # compare with the token in the database
+    username = Auhthorize.get_jwt_subject()
+    token, success = cache.elem_and_status(username + "_token")
+    if not success:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="token not found"
+        )
+
+    recieved_token = Auhthorize.get_raw_jwt()
+    if token != recieved_token:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="received invalid token"
+        )
+
+    access_token = Auhthorize.create_access_token(username)
     return AccessToken(accessToken=access_token)
