2a3685b1dc53cd695d5f30f45c1028df08fd66e0
skeesh24 <d.pacooloff@gmail.com>
2023-09-28 14:58:26 +0500

    postgres entities declared

---
+++ app/database/postgres/entities.py
@@ -0,0 +1,17 @@
+from database.postgres.postgres import metadata, session
+from sqlalchemy import UUID, Column, String, Table, text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class PostgresUser(Base):
+    __tablename__ = "user"
+    id = Column(
+        UUID(False),
+        nullable=False,
+        primary_key=True,
+        server_default=text("gen_random_uuid()"),
+    )
+    username: str = Column(String(length=50), nullable=False, unique=True)
+    password: str = Column(String(length=100), nullable=False)
