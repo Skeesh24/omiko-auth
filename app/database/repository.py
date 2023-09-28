6ba49321218417dd959f3f6e56bb3aebee916b2b
skeesh24 <d.pacooloff@gmail.com>
2023-09-28 15:01:26 +0500

    Postgres repository added

--- app/database/repository.py
+++ app/database/repository.py
@@ -1,17 +1,24 @@
+from typing import Union
+
+from classes.interfaces import IRepository
+from classes.validation import FilterModel, UserCreate, UserResponse
+from database.firebase.entities import FireUser
+from database.firebase.firebase import get_db
+from database.postgres.entities import PostgresUser
+from database.postgres.postgres import get_session
 from fastapi import HTTPException, status
 from google.cloud.firestore_v1.base_document import DocumentSnapshot
+from sqlalchemy import text
 
-from ...classes.validation import FilterModel, UserCreate, UserResponse
-from ...database.firebase.firebase import get_db
-from ...database.firebase.entities import User
+CREATE_USER_TABLE = "CALL create_user_table()"
 
 
-class UserFirebase:
+class UserFirebase(IRepository):
     def __init__(self) -> None:
         self.users = get_db().collection("user")
         self.db = get_db()
 
-    def get(self, limit: int = 5, offset: int = 0, where: FilterModel = None) -> dict:
+    def get(self, limit: int = 5, offset: int = 0, **kwargs) -> dict:
         """
         ## Gets a document by id
 
@@ -23,6 +30,9 @@ class UserFirebase:
         ### returns a list of documents
         """
         query = self.users
+
+        where = None  # where parameter needs to be constructed automatically
+        raise NotImplementedError()
 
         try:
             if where:
@@ -68,7 +78,7 @@ class UserFirebase:
                 status.HTTP_409_CONFLICT, "this username is already in use"
             )
 
-        new_elem = User(**element.dict(exclude_defaults=True))
+        new_elem = FireUser(**element.dict(exclude_defaults=True))
         new_elem.save()
         return new_elem.to_dict()
 
@@ -102,3 +112,58 @@ class UserFirebase:
         """
 
         self.db.recursive_delete(self.users.document(user.id))
+
+
+class UserPostgres(IRepository):
+    def __init__(self) -> None:
+        self.db = get_session()
+
+    def get_users(self):
+        self.db.execute(text(CREATE_USER_TABLE))
+        return self.db.query(PostgresUser)
+
+    def get(
+        self, limit: int = 5, offset: int = 0, **kwargs
+    ) -> Union[list[PostgresUser], PostgresUser]:
+        query = self.get_users()
+
+        if len(kwargs) > 0:
+            try:
+                for k, v in kwargs.items():
+                    query = query.filter(
+                        PostgresUser.__getattribute__(PostgresUser, k) == v
+                    )
+            except AttributeError:
+                return None
+
+        return (
+            query.limit(limit).offset(offset).all()
+            if limit > 1
+            else query.limit(limit).offset(offset).first()
+        )
+
+    def add(self, user: PostgresUser) -> PostgresUser:
+        try:
+            self.db.add(user)
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        self.db.commit()
+        return user
+
+    def update(self, update_user: PostgresUser) -> PostgresUser:
+        user_id = self.get(1, 0, username=update_user.username)[0].id
+        user = PostgresUser(id=user_id, **update_user.__dict__)
+        try:
+            self.remove(update_user)
+            self.add(user)
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+        self.db.commit()
+        return user
+
+    def remove(self, user: PostgresUser) -> None:
+        try:
+            self.db.delete(user)
+        except Exception as e:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        self.db.commit()
