c173514942fce7ee594e0e06864b33fc053d8579
skeesh24 <d.pacooloff@gmail.com>
2023-09-29 19:40:43 +0500

    rabbitmq and debug vars added

--- app/configuration.py
+++ app/configuration.py
@@ -8,3 +8,6 @@ class Settings(BaseModel):
     authjwt_access_token_expires: int = int(environ.get("JWT_TOKEN_EXPIRATION_MINUTES"))
     authjwt_refresh_token_expires: int = int(environ.get("JWT_TOKEN_EXPIRATION_DAYS"))
     REDIS_EXTERNAL: str = environ.get("REDIS_EXTERNAL")
+    RECOVERY_QUEUE: str = environ.get("RECOVERY_QUEUE")
+    BROKER_HOST: str = environ.get("BROKER_HOST")
+    DEBUG: str = environ.get("DEBUG")
