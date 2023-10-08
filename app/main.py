6dcc423344a6913b5ad15c9e88699f6d551fc297
Daniel <108313445+Skeesh24@users.noreply.github.com>
2023-10-08 23:56:19 +1000

    cors updated.py

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://omiko-proxy.onrender.com"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_headers=["*"],
     allow_methods=["*"],
