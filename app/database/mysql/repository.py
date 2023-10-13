19527543548cd7c6e4237965cea653521f5667cd
skeesh24 <d.pacooloff@gmail.com>
2023-10-13 18:19:41 +0500

    updatee add query

--- app/database/mysql/repository.py
+++ app/database/mysql/repository.py
@@ -43,7 +43,7 @@ class UserMysql(IRepository):
 
     def add(self, user: DatabaseUserInsert) -> DatabaseUser:
         try:
-            self.db.add(user)
+            self.db.query(DatabaseUserInsert).add_entity(user)
         except Exception as e:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
         self.db.commit()
