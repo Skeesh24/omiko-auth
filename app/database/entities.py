1d3df8a5166068fb4898369a96aa2a8712b068f6
skeesh24 <d.pacooloff@gmail.com>
2023-10-13 17:33:13 +0500

    replace config and entities

--- app/database/entities.py
+++ app/database/entities.py
@@ -1,18 +1,28 @@
-from database.postgres.config import sett
-from database.postgres.postgres import metadata, session
+from database.config import sett
+from database.database import metadata, session
 from sqlalchemy import UUID, Column, String, Table, text
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
-class PostgresUser(Base):
+class DatabaseUser(Base):
     __tablename__ = sett.USER_TABLENAME
     id = Column(
         UUID(False),
         nullable=False,
         primary_key=True,
         server_default=text(sett.ID_SERVER_DEFAULT),
+    )
+    username: str = Column(String(length=50), nullable=False, unique=True)
+    password: str = Column(String(length=100), nullable=False)
+
+
+class DatabaseUserInsert(Base):
+    __tablename__ = sett.USER_TABLENAME
+    id = Column(
+        UUID(False),
+        nullable=True,
     )
     username: str = Column(String(length=50), nullable=False, unique=True)
     password: str = Column(String(length=100), nullable=False)
