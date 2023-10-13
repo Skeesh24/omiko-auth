3b82b401ba0b83cbae2324623ebf3746d5e01187
skeesh24 <d.pacooloff@gmail.com>
2023-10-13 15:41:55 +0500

    add env for the user table create procedure calling

--- app/database/postgres/config.py
+++ app/database/postgres/config.py
@@ -11,3 +11,5 @@ class sett:
     POSTGRES_HOST: str = environ.get("POSTGRES_HOST")
     POSTGRES_PORT: str = environ.get("POSTGRES_PORT")
     POSTGRES_DBNAME: str = environ.get("POSTGRES_DBNAME")
+    CREATE_USER_TABLE: str = environ.get("CREATE_USER_TABLE")
+
