62f0c929eb524a09c95e0d51f705d324778e588d
skeesh24 <d.pacooloff@gmail.com>
2023-10-14 21:25:49 +0500

    add token type into main configuration

--- app/configuration.py
+++ app/configuration.py
@@ -1,5 +1,6 @@
-from pydantic import BaseModel
 from os import environ
+
+from pydantic import BaseModel
 
 
 class Settings(BaseModel):
@@ -7,3 +8,4 @@ class Settings(BaseModel):
     authjwt_algorithm: str = environ.get("JWT_ALGORYTHM")
     authjwt_access_token_expires: int = int(environ.get("JWT_TOKEN_EXPIRATION_MINUTES"))
     authjwt_refresh_token_expires: int = int(environ.get("JWT_TOKEN_EXPIRATION_DAYS"))
+    authjwt_token_type: str = environ.get("JWT_TOKEN_TYPE")
